95618f3d1eed86a1b5c20219d4cbf2c07a74ec44
april <lqqojin@aprilui-MacBookPro.local>
2020-05-02 02:19:52 +0900

    클래스에서 state 사용법

--- src/App.js
+++ src/App.js
@@ -5,25 +5,38 @@ function App() {
     return (
         <div className="container">
             <h2>React World!</h2>
-            <FuncComp />
-            <ClassComp />
+            <FuncComp initNumber={100}/>
+            <ClassComp initNumber={100}/>
         </div>
     );
 }
 
-function FuncComp() {
+function FuncComp(props) {
     return (
         <div className="container">
             <h2>Function Style component</h2>
+            <p>Number: {props.initNumber}</p>
         </div>
     );
 }
 
 class ClassComp extends React.Component {
+    state = {
+        number: this.props.initNumber * 2
+    }
     render() {
         return (
             <div className="container">
                 <h2>Class Style Component</h2>
+                {/*<p>Number: {this.props.initNumber}</p>*/}
+                <p>Number: {this.state.number}</p>
+                <input type="button" value="random" onClick={
+                    function() {
+                        this.setState({
+                            number: Math.floor(Math.random() * 10000)
+                        })
+                    }.bind(this)
+                }/>
             </div>
         );
     }
