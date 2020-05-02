d5da710f81fa62c9f8175e4109035c0e8962f7b1
april <lqqojin@aprilui-MacBookPro.local>
2020-05-02 13:00:08 +0900

    함수에서 state 사용법 hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
@@ -12,31 +12,72 @@ function App() {
 }
 
 function FuncComp(props) {
+    /**
+     * hook 사용
+     */
+    let numberState = useState(props.initNumber);
+    let number = numberState[0];
+    let setNumber = numberState[1];
+
+    /*let dateState = useState((new Date()).toString());
+    let _date = dateState[0];
+    let setDate = dateState[1];*/
+    var [_date, setDate] = useState((new Date()).toString());
+    console.log('FuncComp')
     return (
         <div className="container">
             <h2>Function Style component</h2>
-            <p>Number: {props.initNumber}</p>
+            <p>Number: {number}</p>
+            <p>Date: {_date}</p>
+            <p>
+                <input type="button" value="random" onClick={
+                    function() {
+                        setNumber(Math.floor(Math.random() * 10000))
+                    }
+                }/>
+            </p>
+            <p>
+                <input type="button" value="date" onClick={
+                    function() {
+                        setDate((new Date()).toString())
+                    }
+                }/>
+            </p>
         </div>
     );
 }
 
 class ClassComp extends React.Component {
     state = {
-        number: this.props.initNumber * 2
+        number: this.props.initNumber,
+        date: (new Date()).toString()
     }
     render() {
+        console.log('ClassComp')
         return (
             <div className="container">
                 <h2>Class Style Component</h2>
                 {/*<p>Number: {this.props.initNumber}</p>*/}
-                <p>Number: {this.state.number}</p>
-                <input type="button" value="random" onClick={
-                    function() {
-                        this.setState({
-                            number: Math.floor(Math.random() * 10000)
-                        })
-                    }.bind(this)
-                }/>
+                <p>Number: { this.state.number }</p>
+                <p>Date: { this.state.date }</p>
+                <p>
+                    <input type="button" value="random" onClick={
+                        function() {
+                            this.setState({
+                                number: Math.floor(Math.random() * 10000)
+                            })
+                        }.bind(this)
+                    }/>
+                </p>
+                <p>
+                    <input type="button" value="date" onClick={
+                        function() {
+                            this.setState({
+                                date: (new Date()).toString()
+                            })
+                        }.bind(this)
+                    }/>
+                </p>
             </div>
         );
     }
